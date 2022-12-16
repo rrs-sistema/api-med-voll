7117dc5d1c701172ea08042a3a2a44ab10b36026
Rivaldo Roberto <rrs.sistema@gmail.com>
2022-12-16 11:53:02 -0300

    Incluindo o crud de pacientes

---
+++ src/main/java/med/voll/api/domain/parciente/Asss.java
@@ -0,0 +1,5 @@
+package med.voll.api.domain.parciente;
+
+public class Asss {
+
+}
---
+++ src/main/java/med/voll/api/domain/parciente/DadosAtualizacaoPaciente.java
@@ -0,0 +1,12 @@
+package med.voll.api.domain.parciente;
+
+import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.endereco.DadosEndereco;
+
+public record DadosAtualizacaoPaciente(
+        @NotNull
+        Long id,
+        String nome,
+        String telefone,
+        DadosEndereco endereco) {
+}
